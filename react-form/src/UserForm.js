ec18adb9632813ea6e9541081d2f7565c7e5bf93
Alex <aleksandr.plakhotniy.fmc@gmail.com>
2020-12-13 13:50:39 +0200

    hw done

--- react-form/src/UserForm.js
+++ react-form/src/UserForm.js
@@ -10,7 +10,12 @@ export default class UserForm extends PureComponent {
         email: this.props.user.email,
         password: this.props.user.password,
         phones: this.props.user.phones,
-        validation: {}
+        validation: {
+            name: null,
+            email: null,
+            password: null,
+            phones: []
+        }
     }
 
     inputHandler = (value, typeField) => {
@@ -35,41 +40,105 @@ export default class UserForm extends PureComponent {
 
     handlerSubmit = (e) => {
         e.preventDefault();
-        const allFields = [
 
+        const {name, email, password, phones} = this.state;
+        const allFields = [
+            {field: 'name', value: name},
+            {field: 'email', value: email},
+            {field: 'password', value: password},
+            {field: 'phones', value: phones}
         ]
+        allFields.forEach(item => {
+            if (item.field !== 'phones') {
+                const {field, result} = this.validateFields(item);
+                this.setState(state => {
+                    return {
+                        ...state,
+                        validation: {
+                            ...state.validation,
+                            [field]: result
+                        }
+                    }
+                })
+            } else {
+                const phonesValidation = [];
+                item.value.forEach(({type, number}) => {
+                    const formattedObject = {field: type, value: number}
+                    const {result} = this.validateFields(formattedObject);
+                    phonesValidation.push(result)
+                })
+                this.setState(state => {
+                    return {
+                        ...state,
+                        validation: {
+                            ...state.validation,
+                            phones: [...phonesValidation]
+                        }
+                    }
+                })
+            }
+        })
     }
 
-    validateFields = (value, type) => {
+    validateFields = ({field, value}) => {
         const nameRegEx = /^([А-Яа-яёЁЇїІіЄєҐґ]+\s+){2}[А-Яа-яёЁЇїІіЄєҐґ]+$/;
         const EmailRegEx = /^([a-zA-Z0-9]|(?!\.)[a-zA-Z0-9-.]{1,}[a-zA-Z0-9-])@([a-zA-Z0-9-]+[.]+[a-zA-Z0-9-]+)+$/;
-        const passRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/
+        const passRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
+        const homeRegEx = /^[1-9]{6}$/;
+        const mobileRegEx = /^(0[0-9]{9}|3[0-9]{11})$/;
 
-        switch (type) {
+        switch (field) {
             case 'name':
-                return
+                return {
+                    field: field,
+                    result: nameRegEx.test(value)
+                }
+            case 'email':
+                return {
+                    field: field,
+                    result: EmailRegEx.test(value)
+                }
+            case 'password':
+                return {
+                    field: field,
+                    result: passRegEx.test(value)
+                }
+            case 'home':
+                return {
+                    field: field,
+                    result: homeRegEx.test(value)
+                }
+            case 'mobile':
+                return {
+                    field: field,
+                    result: mobileRegEx.test(value)
+                }
         }
     }
 
     addNumber = () => {
-        this.setState(state => ({phones: [{number: '', type: 'home'}, ...state.phones]}))
+        this.setState(state => ({phones: [...state.phones, {number: '', type: 'home'}]}))
     }
 
     deleteNumber = (index) => {
         this.setState(state => {
             const tempPhones = state.phones
+            const tempPhonesValidation = state.validation.phones
             tempPhones.splice(index, 1)
+            tempPhonesValidation.splice(index, 1)
             return {
                 ...state,
-                phones: [...tempPhones]
+                phones: [...tempPhones],
+                validation: {
+                    ...state.validation,
+                    phones: [...tempPhonesValidation]
+                }
             }
         })
     }
 
-
     render() {
-        console.log(this.state.phones)
-        const {name, email, password} = this.state;
+        const {name, email, password, validation} = this.state;
         return (
             <div className="wrapper">
                 <div className="inputs-group">
@@ -77,16 +146,22 @@ export default class UserForm extends PureComponent {
                         <Input placeholder="Name"
                                value={name}
                                type="text"
+                               style={validation.name === false ? {background: '#F9D0C4', borderColor: 'transparent'}:
+                                   validation.name === true ? {background: '#C2E0C6', borderColor: 'transparent'}: null}
                                onChange={(e) => this.inputHandler(e.target.value, 'name')}
                         />
                         <Input placeholder="Email"
                                value={email}
-                               type="email"
+                               type="text"
+                               style={validation.email === false ? {background: '#F9D0C4', borderColor: 'transparent'}:
+                                   validation.email === true ? {background: '#C2E0C6', borderColor: 'transparent'}: null}
                                onChange={(e) => this.inputHandler(e.target.value, 'email')}
                         />
                         <Input placeholder="Password"
                                value={password}
                                type="password"
+                               style={validation.password === false ? {background: '#F9D0C4', borderColor: 'transparent'}:
+                                   validation.password === true ? {background: '#C2E0C6', borderColor: 'transparent'}: null}
                                onChange={(e) => this.inputHandler(e.target.value, 'password')}
                         />
                         <div className="">
@@ -97,6 +172,8 @@ export default class UserForm extends PureComponent {
                                             <Input placeholder="Phone number"
                                                    type="phone"
                                                    value={item.number}
+                                                   style={validation.phones[index] === false ? {background: '#F9D0C4', borderColor: 'transparent'}:
+                                                       validation.phones[index] === true ? {background: '#C2E0C6', borderColor: 'transparent'}: null}
                                                    onChange={(e) => this.phoneHandler(e.target.value, 'number', index)}
                                             />
                                             <Select value={item.type} onChange={(value) => this.phoneHandler(value, 'type', index)}>
